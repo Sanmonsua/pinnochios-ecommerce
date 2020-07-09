6b225ba9ae15d85d3edfb7ac102c1cdaa524a867
Sanmonsua <sanmonsua@gmail.com>
2020-07-08 22:28:46 -0500

    Add initial models squeme

--- pizza/orders/admin.py
+++ pizza/orders/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Dish, Category, Topping
 
 # Register your models here.
+admin.site.register(Dish)
+admin.site.register(Category)
+admin.site.register(Topping)
--- pizza/orders/models.py
+++ pizza/orders/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=64)
+    smallPrice = models.FloatField()
+    largePrice = models.FloatField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.category}"
+
+
+class Topping(models.Model):
+    name = models.CharField(max_length=64)
+    dishes = models.ManyToManyField(Dish, blank=True, related_name='toppings')
+
+    def __str__(self):
+        return f"{self.name}"

d2c690fb9850bd2f095e7282e6847cf8493d6b5e
Sanmonsua <sanmonsua@gmail.com>
2020-07-10 21:44:47 -0500

    Add : views to login and auth system for the site

--- pizza/orders/urls.py
+++ pizza/orders/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index, do_login, do_logout, signup
 
 urlpatterns = [
-    path('', index)
+    path('', index),
+    path('signup', signup),
+    path('login', do_login),
+    path('logout', do_logout)
 ]
--- pizza/orders/views.py
+++ pizza/orders/views.py
@@ -1,10 +1,60 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, logout, login
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.shortcuts import render, redirect
 from .models import Category, Product, Topping
 
 
 def index(request):
-    categories = Category.objects.all()
+    if request.user.is_authenticated:
+        categories = Category.objects.all()
+        context = {
+            "categories" : categories
+        }
+        return render(request, 'orders/index.html', context=context)
+
+    return redirect('/login')
+
+
+def do_login(request):
+    #This view logs in the user
+    form = AuthenticationForm()
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('/')
+
     context = {
-        "categories" : categories
+        "form":form
     }
-    return render(request, 'orders/index.html', context=context)
+
+    return render(request, 'orders/login.html', context=context)
+
+
+def signup(request):
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            user = form.save()
+            if user is not None:
+                login(request, user)
+                return redirect('/')
+
+    context = {
+        "form":form
+    }
+    return render(request, 'orders/signup.html', context=context)
+
+
+def do_logout(request):
+    logout(request)
+    return redirect('/')

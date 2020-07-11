a09eed7d1c56eeea788c14b9c1aabde8e81ec95e
Sanmonsua <sanmonsua@gmail.com>
2020-07-10 23:36:24 -0500

    Add django-widget-tweaks module to render custom form attributes

--- pizza/pizza/settings.py
+++ pizza/pizza/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'orders.apps.OrdersConfig'
+    'orders.apps.OrdersConfig',
+    'widget_tweaks'
 ]
 
 MIDDLEWARE = [

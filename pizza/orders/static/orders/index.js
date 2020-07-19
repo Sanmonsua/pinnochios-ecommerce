f78af31593899875525171cbc2671f3deea26e79
Sanmonsua <sanmonsua@gmail.com>
2020-07-18 22:32:59 -0500

    Add : toppings section for the modal, using handlebars templates

--- pizza/orders/static/orders/index.js
+++ pizza/orders/static/orders/index.js
@@ -1,5 +1,12 @@
+const toppingTemplate = Handlebars.compile(document.querySelector('#topping').innerHTML);
+const addonTemplate = Handlebars.compile(document.querySelector('#addon').innerHTML);
+
 document.addEventListener('DOMContentLoaded', ()=>{
-  
+  document.querySelectorAll(".product").forEach(p =>{
+    p.onclick = () =>{
+      get_product(p.dataset.product);
+    }
+  })
 });
 
 
@@ -9,7 +16,17 @@ function get_product(product_id){
 
   request.onload = () => {
     const data = JSON.parse(request.responseText);
-    console.log(data);
+    console.log(data)
+    document.querySelector("#product-name").innerHTML = data.name + " - " + data.category;
+    document.querySelector("#product-description").innerHTML = data.description;
+    document.querySelector("#product-small-price").innerHTML = "Small $" + data.smallPrice;
+    document.querySelector("#product-large-price").innerHTML = "Large $" + data.largePrice;
+    let toppingsSection = document.querySelector('#toppings');
+    toppingsSection.innerHTML = "";
+    data.toppings.forEach(t =>{
+      const topping = toppingTemplate({"id":t.id, "name":t.name});
+      toppingsSection.innerHTML += topping;
+    });
   }
 
   request.send();

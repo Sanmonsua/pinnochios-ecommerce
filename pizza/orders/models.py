921f54b69de99c021a8f2c0283435556237fd605
Sanmonsua <sanmonsua@gmail.com>
2020-07-19 21:58:38 -0500

    Edit : total_price field in order model

--- pizza/orders/models.py
+++ pizza/orders/models.py
@@ -62,7 +62,7 @@ class CartItem(models.Model):
 
 class Order(models.Model):
     items = models.ManyToManyField(CartItem, related_name="orders")
-    total_price = models.IntegerField(default=0)
+    total_price = models.FloatField(default=0)
     costumer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="orders", blank=True, default=0)
 
     def __str__(self):

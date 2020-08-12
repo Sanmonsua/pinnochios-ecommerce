59b105398014c223d0bc55e20920d6097b9391ab
Sanmonsua <sanmonsua@gmail.com>
2020-08-11 23:44:16 -0500

    Add whitenoise middleware

--- pizza/pizza/settings.py
+++ pizza/pizza/settings.py
@@ -82,10 +82,14 @@ from decouple import config
 
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
+
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
 
 
 # Password validation

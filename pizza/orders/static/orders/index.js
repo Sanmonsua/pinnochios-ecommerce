2cca64e69c449d3b47da47e4b2aff26c005d7a75
Sanmonsua <sanmonsua@gmail.com>
2020-09-30 18:09:17 -0500

    refactors to improve readability

--- pizza/orders/static/orders/index.js
+++ pizza/orders/static/orders/index.js
@@ -1,167 +1,233 @@
-const toppingTemplate = Handlebars.compile(document.querySelector('#topping').innerHTML);
-const addonTemplate = Handlebars.compile(document.querySelector('#addon').innerHTML);
-const cartItemTemplate = Handlebars.compile(document.querySelector('#cart-item').innerHTML);
+const toppingTemplate = Handlebars.compile(
+	document.querySelector('#topping').innerHTML
+)
+const addonTemplate = Handlebars.compile(
+	document.querySelector('#addon').innerHTML
+)
+const cartItemTemplate = Handlebars.compile(
+	document.querySelector('#cart-item').innerHTML
+)
 
-let maxToppings = 0;
+document.addEventListener('DOMContentLoaded', function onDocumentLoaded() {
+	loadCart()
 
-document.addEventListener('DOMContentLoaded', () => {
+	document.querySelectorAll('.product').forEach(setProductOnClick)
+	document.querySelector('#addToCartButton').onclick = addToCart
 
-  load_cart();
+	function setProductOnClick(product) {
+		product.onclick = function productOnClick() {
+			getProduct(product.dataset.product)
+		}
+	}
+})
 
-  document.querySelectorAll(".product").forEach(p => {
-    p.onclick = () => {
-      get_product(p.dataset.product);
-    }
-  });
+function getProduct(productId) {
+	var request = new XMLHttpRequest()
+	request.open('GET', `products/${productId}`)
 
-  document.querySelector('#addToCartButton').onclick = addToCart;
-});
+	request.onload = function onGetProductRequestLoad() {
+		var {
+			image_url,
+			id,
+			name,
+			category,
+			description,
+			smallPrice,
+			largePrice,
+			toppings,
+			addons,
+			max_toppings,
+		} = JSON.parse(request.responseText)
+		document.querySelector('#product-image').setAttribute('src', image_url)
+		document.querySelector('#product-info').setAttribute('data-id', id)
+		document.querySelector(
+			'#product-name'
+		).innerHTML = `${name} - ${category}`
+		document.querySelector('#product-description').innerHTML = description
+		document.querySelector(
+			'#product-small-price-label'
+		).innerHTML = `Small $${smallPrice}`
+		document.querySelector('#product-small-price-radio').value = smallPrice
+		document.querySelector(
+			'#product-large-price-label'
+		).innerHTML = `Large $${largePrice}`
+		document.querySelector('#product-large-price-radio').value = largePrice
 
+		createToppingsSection()
+		createAddOnsSection()
+		createCheckboxes()
 
-function get_product(product_id){
-  const request = new XMLHttpRequest();
-  request.open("GET", `products/${product_id}`);
+		// ---------------------------------------------------
 
-  request.onload = () => {
-    const data = JSON.parse(request.responseText);
-    document.querySelector('#product-image').setAttribute('src', data.image_url)
-    document.querySelector("#product-info").setAttribute('data-id', data.id)
-    document.querySelector("#product-name").innerHTML = data.name + " - " + data.category;
-    document.querySelector("#product-description").innerHTML = data.description;
-    document.querySelector("#product-small-price-label").innerHTML = "Small $" + data.smallPrice;
-    document.querySelector("#product-small-price-radio").value = data.smallPrice;
-    document.querySelector("#product-large-price-label").innerHTML = "Large $" + data.largePrice;
-    document.querySelector("#product-large-price-radio").value = data.largePrice;
-    let toppingsSection = document.querySelector('#toppings');
-    toppingsSection.innerHTML = "";
-    if (data.toppings.length > 0){
-      document.querySelector("#toppings-section").style.display = "block";
-      data.toppings.forEach(t =>{
-        const topping = toppingTemplate({"id":t.id, "name":t.name});
-        toppingsSection.innerHTML += topping;
-      });
-    } else {
-      document.querySelector("#toppings-section").style.display = "none";
-    }
-    let addonsSection = document.querySelector('#addons');
-    addonsSection.innerHTML = "";
-    if (data.addons.length > 0) {
-      document.querySelector("#addons-section").style.display = "block";
-      data.addons.forEach(a => {
-        const addon = addonTemplate({"id": a.id, "name": a.name, "price": a.price});
-        addonsSection.innerHTML += addon;
-      });
-    } else {
-      document.querySelector("#addons-section").style.display = "none";
-    }
-    maxToppings = data.max_toppings;
-    document.querySelectorAll("input[name='groupOfCheckboxes']").forEach(checkbox => {
-      checkbox.addEventListener('change', function () {
-        if (document.querySelectorAll("input[name='groupOfCheckboxes']:checked").length > maxToppings) {
-          checkbox.checked = false;
-        }
-      });
-    });
+		function createToppingsSection() {
+			var toppingsSection = document.querySelector('#toppings')
+			toppingsSection.innerHTML = ''
+			if (toppings.length > 0) {
+				document.querySelector('#toppings-section').style.display =
+					'block'
+				toppings.forEach(createToppingElement)
+			} else {
+				document.querySelector('#toppings-section').style.display =
+					'none'
+			}
 
-  }
+			function createToppingElement({ id, name }) {
+				var toppingElement = toppingTemplate({ id, name })
+				toppingsSection.innerHTML += toppingElement
+			}
+		}
 
-  request.send();
+		function createAddOnsSection() {
+			var addonsSection = document.querySelector('#addons')
+			addonsSection.innerHTML = ''
+			if (addons.length > 0) {
+				document.querySelector('#addons-section').style.display =
+					'block'
+				addons.forEach(createAddOnElement)
+			} else {
+				document.querySelector('#addons-section').style.display = 'none'
+			}
+
+			function createAddOnElement({ id, name, price }) {
+				var addOn = addonTemplate(id, name, price)
+				addonsSection.innerHTML += addOn
+			}
+		}
+
+		function createCheckboxes() {
+			document
+				.querySelectorAll("input[name='groupOfCheckboxes']")
+				.forEach(createToppingsCheckbox)
+
+			function createToppingsCheckbox(checkbox) {
+				checkbox.addEventListener('change', selectToppingCheckbox)
+			}
+			function selectToppingCheckbox() {
+				if (
+					document.querySelectorAll(
+						"input[name='groupOfCheckboxes']:checked"
+					).length > max_toppings
+				) {
+					checkbox.checked = false
+				}
+			}
+		}
+	}
+
+	request.send()
 }
 
-function load_cart() {
-  const request = new XMLHttpRequest();
-  request.open('GET', 'cart')
+function loadCart() {
+	var request = new XMLHttpRequest()
+	request.open('GET', 'cart')
 
-  request.onload = () =>{
-    const data = JSON.parse(request.responseText);
-    const cart = data.cart;
-    document.querySelector('#cart-count').innerHTML = cart.length;
-    document.querySelector('#cart-items-list').innerHTML = "";
-    let total_price = 0;
-    if (cart.length > 0){
-      document.querySelector('#cart-empty-label').setAttribute("hidden", "true");
-      document.querySelector('#clear-cart').removeAttribute("disabled");
-      document.querySelector('#checkout').removeAttribute("disabled");
-      cart.forEach( item =>{
-        total_price += item.price;
-        const item_content = cartItemTemplate(item);
-        document.querySelector('#cart-items-list').innerHTML += item_content;
-      })
-    } else {
-      document.querySelector('#cart-empty-label').removeAttribute("hidden");
-      document.querySelector('#clear-cart').disabled = "true";
-      document.querySelector('#checkout').disabled = "true";
-    }
+	request.onload = function onLoadCartRequest() {
+		var { cart } = JSON.parse(request.responseText)
+		document.querySelector('#cart-count').innerHTML = cart.length
+		document.querySelector('#cart-items-list').innerHTML = ''
+		var totalPrice = 0
+		if (cart.length > 0) {
+			document
+				.querySelector('#cart-empty-label')
+				.setAttribute('hidden', 'true')
+			document.querySelector('#clear-cart').removeAttribute('disabled')
+			document.querySelector('#checkout').removeAttribute('disabled')
+			cart.forEach(loadItem)
+		} else {
+			document
+				.querySelector('#cart-empty-label')
+				.removeAttribute('hidden')
+			document.querySelector('#clear-cart').disabled = 'true'
+			document.querySelector('#checkout').disabled = 'true'
+		}
 
-    document.querySelector('#checkout').innerHTML = `Checkout $ ${total_price}`;
+		function loadItem(item) {
+			totalPrice += item.price
+			var itemElement = cartItemTemplate(item)
+			document.querySelector('#cart-items-list').innerHTML += itemElement
+		}
 
-  }
+		document.querySelector(
+			'#checkout'
+		).innerHTML = `Checkout $ ${totalPrice}`
+	}
 
-  request.send()
+	request.send()
 }
-
 
 function closeAllModals() {
+	var modals = document.getElementsByClassName('modal')
+	modals.forEach(closeModal)
 
-    const modals = document.getElementsByClassName('modal');
+	var modalsBackdrops = document.getElementsByClassName('modal-backdrop')
+	modalsBackdrops.forEach(modalBackdropRemove)
 
-    for(let i=0; i<modals.length; i++) {
-      modals[i].classList.remove('show');
-      modals[i].setAttribute('aria-hidden', 'true');
-      modals[i].setAttribute('style', 'display: none');
-    }
+	function closeModal(modal) {
+		modal.classList.remove('show')
+		modal.setAttribute('aria-hidden', 'true')
+		modal.setAttribute('style', 'display: none')
+	}
 
-    const modalsBackdrops = document.getElementsByClassName('modal-backdrop');
-
-    for(let i=0; i<modalsBackdrops.length; i++) {
-      document.body.removeChild(modalsBackdrops[i]);
-    }
+	function modalBackdropRemove(modalBackdrop) {
+		document.body.removeChild(modalBackdrop)
+	}
 }
 
+function addToCart() {
+	var request = new XMLHttpRequest()
+	request.open('POST', 'addtocart')
 
-function addToCart(){
-  const request = new XMLHttpRequest();
-  request.open('POST', 'addtocart')
+	request.onload = function onAddToCartRequest() {
+		loadCart()
+		closeAllModals()
+	}
 
-  request.onload = () =>{
-      load_cart();
-      closeAllModals();
-  }
+	var data = new FormData()
+	var price = 0
 
-  const data = new FormData();
-  let price = 0;
-  const product_id = document.querySelector('#product-info').dataset.id;
-  data.append('product_id', product_id);
+	var productId = document.querySelector('#product-info').dataset.id
+	data.append('product_id', productId)
 
-  const quantity = 1;
-  data.append('quantity', quantity);
+	var quantity = 1
+	data.append('quantity', quantity)
 
-  let toppings = [];
-  document.querySelectorAll('.topping').forEach( t =>{
-    if (t.checked){
-      toppings.push(t.id);
-    }
-  });
-  data.append('toppings', toppings);
+	var toppings = []
+	document.querySelectorAll('.topping').forEach(addTopping)
+	data.append('toppings', toppings)
 
-  let addons = [];
-  document.querySelectorAll('.addon').forEach( a =>{
-    if (a.checked){
-      addons.push(a.id)
-      price += parseFloat(a.dataset.price);
-    }
+	var addons = []
+	document.querySelectorAll('.addon').forEach(addAddon)
+	data.append('addons', addons)
 
-  });
-  data.append('addons', addons);
+	document.querySelectorAll('.size-radio').forEach(applySizeToPrice)
 
-  document.querySelectorAll('.size-radio').forEach( s =>{
-    if (s.checked){
-      price += parseFloat(s.value);
-    }
-  });
-  data.append('price', price);
-  data.append('csrfmiddlewaretoken', window.CSRF_TOKEN);
+	data.append('price', price)
+	data.append('csrfmiddlewaretoken', window.CSRF_TOKEN)
 
-  request.send(data);
-  return false;
+	request.send(data)
+	return false
+
+	// -------------------------------------------------------------
+
+	function addTopping(topping) {
+		var { id, checked } = topping
+		if (checked) {
+			toppings.push(id)
+		}
+	}
+
+	function addAddon(addOn) {
+		var { checked, id } = addOn
+		if (checked) {
+			addons.push(id)
+			price += Number(addOn.dataset.price)
+		}
+	}
+
+	function applySizeToPrice(sizeOption) {
+		var { checked, value } = sizeOption
+		if (checked) {
+			price += Number(value)
+		}
+	}
 }
